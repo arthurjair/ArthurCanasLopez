38a6980d402d23513020608fb3955903d9268d8e
arthu <i532632.fhict.nl>
2025-10-30 15:36:48 +0100

    Adding assets for portfolio

---
+++ assets/assets.js
@@ -0,0 +1,38 @@
+import hero_image from './hero.png';
+import hero_swag from './hero-swag.jpg';
+import icon_aboutme from './icon-aboutme.png';
+import icon_home from './icon-home.png';
+import icon_projects from './icon-projects.png';
+import menu_burger from './menu-burger.png';
+import menu_close from './menu-close.png';
+import logo_portfolio from './logo-portfolio.png';
+
+export const assets = {
+  hero_image,
+  hero_swag,
+  icon_aboutme,
+  icon_home,
+  icon_projects,
+  menu_burger,
+  menu_close,
+  logo_portfolio,
+};
+
+export const workData = [
+  {
+    title: 'HeyYou',
+    image: './work-heyyou.png',
+  },
+    {   
+    title: 'Boroughs',
+    image: './work-boroughs.jpg',
+  },
+  {
+    title: 'Vanderlande Trend App',
+    image: './work-vanderlande.jpg',
+  },
+  {
+    title: 'Liliana Erasmus',
+    image: './work-liliana.jpg',
+  },
+];
