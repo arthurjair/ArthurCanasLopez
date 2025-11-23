f36ceaaf007c2188d9dac5f89e129f26e6629db7
arthu <i532632.fhict.nl>
2025-11-23 12:08:34 +0100

    footer changes: added links and animation

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,16 +16,27 @@ export default function Skills() {
 
         <div className="relative flex items-center justify-center ">
 
-           <div className="w-full max-w-[95vw] bg-[#15E70D] rounded-lg border-2 border-black-700 mb-5 ">
-              <div className="flex">
-                <ul>
-                    <li className="text-lg text-white">INSTAGRAM</li>
-                    <li className="text-lg text-white">LINKEDIN</li>
-                    <li className="text-lg text-white">PINTEREST</li>
-                    <li className="text-lg text-white">Collaboration With <span>Cua Studio</span> </li>
+           <div className="w-full max-w-[95vw] bg-[#15E70D] rounded-lg border-2 border-black-700 mb-5">
+              
+                <ul className='flex pt-2 pb-2 '>
+                    <div className='flex flex-row gap-4 pl-5 '>
+                     <li className="text-lg text-white"><a href ="https://www.instagram.com/arthurcanaslopez/" className='inline-block hover:scale-110 hover:-translate-y-1 hover:text-blue-600 transition-all duration-300 ease-out' target="_blank">INSTA</a></li>
+                     <li className="text-lg text-white"><a href="https://www.linkedin.com/in/arthur-cañas-11271b328" className='inline-block hover:scale-110 hover:-translate-y-1 hover:text-blue-600 transition-all duration-300 ease-out' target="_blank">LINKEDIN</a></li>
+                     <li className="text-lg text-white"><a href="https://pin.it/2RArftI6v" className='inline-block hover:scale-110 hover:-translate-y-1 hover:text-blue-600 transition-all duration-300 ease-out' target="_blank">PINTEREST</a></li>
+                    </div>
+
+                    <li className='ml-auto pr-5'>
+                        <p className="text-lg text-white ">
+                         Collaboration With <span className='underline'>Cua Studio</span>
+                        </p>
+
+                    </li>
+                    
 
                 </ul>
-              </div>
+
+                
+              
             </div>
         </div>
     </section>
