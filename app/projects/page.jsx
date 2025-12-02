51d0174cb686b5800bfebb0790c9f3b48ab3b8a7
arthu <i532632.fhict.nl>
2025-12-02 15:54:00 +0100

    text change

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -62,9 +62,9 @@ export default function Projects() {
                                   />
                                 ) : (
                                   <div className="w-full rounded-lg bg-white p-6 shadow-[-12px_12px_0_rgba(0,0,0,1)] border-0">
-                                    <h1 className="text-4xl font-bold mb-4">{item.title}</h1>
-                                    <h2 className="text-md font-extralight mb-4">{item.subtitle}</h2>
-                                    <p className="text-md whitespace-pre-line">
+                                    <h1 className="text-5xl font-bold mb-4">{item.title}</h1>
+                                    <h2 className="text-lg font-extralight mb-4">{item.subtitle}</h2>
+                                    <p className="text-xl whitespace-pre-line">
                                       Imagine if your crush actually knew you existed ...well, now they can! With HeyYou, you simply add one of their social media profiles, and the app gathers all their public information for you.{"\n\n"}
                                       HeyYou started as a joke I made a semester earlier, but during my Smart Mobile semester, my teachers actually liked the concept. So my partner and I decided to make it real. The app collects publicly available online information and brings it together in one place, making it easier to “get to know” someone before they even know you.{"\n\n"}
                                       It’s meant to be a funny, satirical project that also raises awareness about how much personal information people share online and how easily accessible it really is.
