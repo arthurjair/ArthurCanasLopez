9d4416b70edc8ab7cf9b5258221ed7ab552b2d99
arthu <i532632.fhict.nl>
2025-12-16 11:39:48 +0100

    GOATED RESPONSIVE INDEX

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -9,33 +9,37 @@ const HeaderSection = () => {
     <section className="relative w-full  ">
       {/* Blue container (now wraps nav + content) */}
       <div className="relative z-20 flex items-center justify-center py-12 md:py-0">
-        <div className="min-w-[95vw] max-h-[90vh]  bg-blue-600 rounded-lg border-2 border-black-700 p-1 mt-5">
+        <div className=" w-full max-w-[95vw]  bg-blue-600 rounded-lg border-2 border-black-700 p-1 mt-5">
           {/* Navbar inside the container (moved to component) */}
           <Navigationbar isInline={true} />
 
           {/* Hero content */}
           
-          <div className="my-4 flex justify-center pt-20 text-center  ">
-            <div>
+          <div className="  justify-center  text-center mt-10 md:mt-40  ">
+            
+            <div className="grid md:grid-cols-2 p-2 ">
+              <div>
+               <h1 className=" md:mt-10 md:mb-10 mb-5  text-6xl scale-y-150 md:text-8xl lg:text-[12vw] font-figtree font-black text-[#F7FD06] [-webkit-text-stroke:1.5px_#000000] md:[-webkit-text-stroke:4px_#000000]  ">
+                 WHAT'S <br />GOOD!
+               </h1>
+               
+              </div>
+              <div>
                
                <Image
                  src={assets.arthur_popout}
                  alt="Hero Image"
-                 width={600}
-                
-                 className="object-contain "
+                 className="object-contain w-70 md:w-90 lg:w-120 mx-auto "
                 />
-             </div>
-            <div className="w-150">
-              <h1 className="text-6xl lg:text-[8.5vw] font-figtree font-black text-[#F7FD06] [-webkit-text-stroke:2px_#000000] ">
-                 WHAT'S <br />GOOD!
-               </h1>
-              <h2 className="text-lg lg:text-[3.5vw] font-figtree font-black text-[#FF0000] [-webkit-text-stroke:2px_#FFFFFF] mt-2 mb-2 flex  ">
-                My name is ARTHUR and I create stuff.
-              </h2>
-              <p className="  text-lg lg:text-3xl font-inter font-black text-[#15E70D] [-webkit-text-stroke:1px_#AC09C9] ">
+                <h2 className="text-3xl p-5 lg:text-4xl font-figtree font-extrabold text-[#FF0000] [-webkit-text-stroke:1px_#FFFFFF] lg:[-webkit-text-stroke:2px_#FFFFFF] ">
+                 My name is ARTHUR and I create stuff.
+                </h2>
+               <p className="  text-md lg:text-3xl font-inter font-black text-[#15E70D] [-webkit-text-stroke:.5px_#AC09C9] lg:[-webkit-text-stroke:1px_#AC09C9]  ">
                 Check out my work below!
-              </p>
+               </p>
+                
+             </div>
+              
             </div>
 
             
@@ -43,7 +47,8 @@ const HeaderSection = () => {
 
           <div className='flex justify-end '>
             <Image src={assets.hero_swag} alt="Swag" 
-            width={75}
+              className="w-10 md:w-15 lg:w-25 pointer-events-none  "
+            
             
             />
 
