c965accb59d0ad74baa0fc47473a8bb27c9124e8
arthu <157164573+arthurjair@users.noreply.github.com>
2026-06-02 13:27:26 +0200

    recent work section

--- app/components/Recentworks.jsx
+++ app/components/Recentworks.jsx
@@ -116,7 +116,7 @@ const HorizontalScrollCarousel = () => {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-38%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-50%"]);
 
   return (
     <section ref={targetRef} className="relative h-[200vh] ">
